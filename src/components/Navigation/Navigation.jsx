63dce544b29130e8781e927f831e12a8145fed5e
Kateryna Hizhevska <167450647+KaterynaHizhevska@users.noreply.github.com>
2024-12-16 10:10:42 +0100

    Navigation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -13,7 +13,7 @@ function Navigation() {
               <NavLink to="/" className={activeNavLink}>
                   Home
               </NavLink>
-              <NavLink>
+              <NavLink to="" className={activeNavLink}>
                   Movies
               </NavLink>
           </nav>
